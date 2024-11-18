961e85f888667191a1ecc6faffc91601f3281a19
Miika Holmlund <miika.holmlund@gmail.com>
2024-11-18 16:11:41 +0200

    add login page

--- galler/urls.py
+++ galler/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include("imggallery.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("admin/", admin.site.urls),
 ]
