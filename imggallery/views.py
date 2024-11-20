17e2089ca2ca9c4923b8e718b8bf81deaf7a9500
Miika Holmlund <miika.holmlund@gmail.com>
2024-11-20 12:48:03 +0200

    implement file storing on the server

--- imggallery/views.py
+++ imggallery/views.py
@@ -1,11 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+import os
 
 @login_required
 def mainView(request):
     return render(request, "index.html")
 
+def store_file(file, filename, user):
+   name = user.username + "/" + filename
+   os.makedirs(os.path.dirname(name), exist_ok=True)
+   with open(name, "wb") as f:
+       f.write(file.read())
+
+@csrf_exempt
 @login_required
 def uploadView(request):
+    if request.method == "POST":
+        user = request.user
+        file = request.FILES['file']
+        filename = request.POST.get("name")
+        store_file(file, filename, user)
+        return redirect("/")
+
     return render(request, "upload.html")
